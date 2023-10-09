82ab9db662017454820a970e1876350adfe1d5a9
Francois Polo <andasan@gmail.com>
2023-10-08 20:18:55 -0700

    complete crud operations on api

--- server/src/api/routes/product.ts
+++ server/src/api/routes/product.ts
@@ -17,4 +17,68 @@ export default (app: Router, { products }: { products: IProduct[] }) => {
         },
     );
 
+    productRoute.get(
+        '/:id',
+        async (req: Request, res: Response, next: NextFunction) => {
+            try {
+                const id = parseInt(req.params.id);
+                const product = products.find(product => product.productId === id);
+                if (!product) {
+                    return res.status(404).json({ message: 'Product not found' });
+                }
+                return res.status(200).json({ product });
+            } catch (err) {
+                next(err);
+            }
+        },
+    );
+
+    productRoute.post(
+        '/',
+        async (req: Request, res: Response, next: NextFunction) => {
+            try {
+                const product = req.body.product;
+                products.push(product);
+                return res.status(200).json({ product });
+            } catch (err) {
+                next(err);
+            }
+        },
+    );
+
+    productRoute.put(
+        '/:id',
+        async (req: Request, res: Response, next: NextFunction) => {
+            try {
+                const id = parseInt(req.params.id);
+                const product = req.body.product;
+                const index = products.findIndex(product => product.productId === id);
+                if (index === -1) {
+                    return res.status(404).json({ message: 'Product not found' });
+                }
+                products[index] = product;
+                return res.status(200).json({ product });
+            } catch (err) {
+                next(err);
+            }
+        },
+    );
+
+    productRoute.delete(
+        '/:id',
+        async (req: Request, res: Response, next: NextFunction) => {
+            try {
+                const id = parseInt(req.params.id);
+                const index = products.findIndex(product => product.productId === id);
+                if (index === -1) {
+                    return res.status(404).json({ message: 'Product not found' });
+                }
+                products.splice(index, 1);
+                return res.status(200).json({ message: 'Product deleted' });
+            } catch (err) {
+                next(err);
+            }
+        },
+    );
+
 };
